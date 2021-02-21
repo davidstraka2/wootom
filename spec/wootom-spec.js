ce32a6d22130d7c76b96fdb87dd04054a1bb2cc4
David Straka <davidstraka.public@gmail.com>
2021-02-21 22:42:59 +0100

    Add Hello World test [#4]
    Tests that a notification with the correct "hello" message is shown upon
    triggering the "hello" command.
    
    How to run the test:
    https://flight-manual.atom.io/hacking-atom/sections/writing-specs/#running-specs

---
+++ spec/wootom-spec.js
@@ -0,0 +1,42 @@
+'use babel';
+
+import wootom from '../lib/wootom';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Wootom', () => {
+    let workspaceElement, activationPromise;
+
+    beforeEach(() => {
+        workspaceElement = atom.views.getView(atom.workspace);
+        activationPromise = atom.packages.activatePackage('wootom');
+    });
+
+    describe('when the wootom:hello event is triggered', () => {
+        it('shows a notification', () => {
+            // List all past notifications
+            const listNotificationMessages = () => atom.notifications
+                .getNotifications()
+                .map(notification => notification.getMessage());
+
+            // Before the activation event the notification with "hello" message
+            // from Wootom has not been shown
+            expect(listNotificationMessages())
+                .not.toContain(wootom.notificationMessage);
+
+            // This is an activation event, triggering it will cause the package
+            // to be activated.
+            atom.commands.dispatch(workspaceElement, 'wootom:hello');
+
+            waitsForPromise(() => activationPromise);
+
+            runs(() => {
+                expect(listNotificationMessages())
+                    .toContain(wootom.notificationMessage);
+            });
+        });
+    });
+});
