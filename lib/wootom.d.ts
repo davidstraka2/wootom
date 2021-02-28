93cdfe6388e1ed739b39def87102f4c3a23b7c67
David Straka <davidstraka.public@gmail.com>
2021-02-28 16:06:27 +0100

    Publish v0.1.0 [#13]

---
+++ lib/wootom.d.ts
@@ -0,0 +1,5 @@
+export declare const notificationMessage = "Wootom was called and says hello!";
+export declare function activate(): void;
+export declare function deactivate(): void;
+export declare function serialize(): void;
+export declare function hello(): void;
