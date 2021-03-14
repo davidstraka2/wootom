c8f6525c7ad0f18eff38213826b54435dd7bdaaa
David Straka <davidstraka.public@gmail.com>
2021-03-15 00:08:50 +0100

    Add pack script and npm job [#19]
    ... which prepares a production package in the /dist/ directory.

---
+++ scripts/pack.mjs
@@ -0,0 +1,33 @@
+import {copy, mkdirs, pathExists} from 'fs-extra';
+import {resolve} from 'path';
+
+const distDir = 'dist';
+
+/**
+ * Copy file or directory to distDir
+ *
+ * @param {string} src Path to the file or directory to be copied
+ * @param {string} [dest] The path to be resolved from distDir, where should src
+ * be copied to. Leave undefined to use the src path.
+ * @returns Promise which resolves after the copy operation is finished
+ */
+const cp = async (src, dest) => {
+    if (!(await pathExists(src))) return;
+    if (typeof dest === 'undefined') dest = src;
+    await copy(src, resolve(distDir, dest));
+};
+
+// Ensure that distDir and any parent directories exist
+await mkdirs(distDir);
+
+await Promise.all([
+    cp('grammars/'),
+    cp('lib/'),
+    cp('spec/'),
+    cp('.gitattributes'),
+    cp('.gitignore-dist', '.gitignore'),
+    cp('CHANGELOG.md'),
+    cp('LICENSE.txt'),
+    cp('package.json'),
+    cp('README.md'),
+]);
