a903482ec3f2d9296abb8bf9d32526fe6e8b483b
David Straka <davidstraka.public@gmail.com>
2021-05-13 23:30:05 +0200

    Publish v0.6.0 [#40]

--- lib/template/renderers/inner-env/eqref.d.ts
+++ lib/template/renderers/inner-env/eqref.d.ts
@@ -2,8 +2,9 @@ import { ASTNode } from '../../../core/ast/ast-node';
 import { Renderer } from '../../../core/rendering/renderer';
 import { RenderingManager } from '../../../core/rendering/rendering-manager';
 import { WooElementKind } from '../../../util/types/woo';
-export declare class InnerEnvUReferenceRenderer implements Renderer {
+/** Renderer of the eqref inner environment */
+export declare class InnerEnvEqrefRenderer implements Renderer {
     readonly kind: WooElementKind;
-    readonly abstractVariant = "_reference";
+    readonly abstractVariant = "eqref";
     render(renderingManager: RenderingManager, astNode: ASTNode): Node;
 }
--- lib/template/renderers/inner-env/footnote.d.ts
+++ lib/template/renderers/inner-env/footnote.d.ts
@@ -2,8 +2,9 @@ import { ASTNode } from '../../../core/ast/ast-node';
 import { Renderer } from '../../../core/rendering/renderer';
 import { RenderingManager } from '../../../core/rendering/rendering-manager';
 import { WooElementKind } from '../../../util/types/woo';
-export declare class InnerEnvUReferenceRenderer implements Renderer {
+/** Renderer of the footnote inner environment */
+export declare class InnerEnvFootnoteRenderer implements Renderer {
     readonly kind: WooElementKind;
-    readonly abstractVariant = "_reference";
+    readonly abstractVariant = "footnote";
     render(renderingManager: RenderingManager, astNode: ASTNode): Node;
 }
